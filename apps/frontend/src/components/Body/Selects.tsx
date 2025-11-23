f824d2443cc82094fb27d02f5afd59c2c10fd6c3
sovan.dey1@vodafone.com <sovan.dey1@vodafone.com>
2025-11-23 19:11:24 +0530

    JSON style pushed

--- apps/frontend/src/components/Body/Selects.tsx
+++ apps/frontend/src/components/Body/Selects.tsx
@@ -1,6 +1,10 @@
 import { Form } from 'react-bootstrap';
 
 const Selects = (props: any) => {
+  const options = require('../../data/property/dropdown/' +
+    props.data.pickOptionValue +
+    '.json');
+
   return (
     <Form.Group>
       <Form.Label>{props.data.label}</Form.Label>
@@ -16,13 +20,17 @@ const Selects = (props: any) => {
           props.data.validation
         )}
       >
-        {props.data.values !== undefined &&
-          props.data.values.map((eachOption: any) => {
-            return <option value={eachOption.value}>{eachOption.label}</option>;
+        {options !== undefined &&
+          options.map((eachOption: any) => {
+            return (
+              <option value={eachOption.value} key={eachOption.value}>
+                {eachOption.label}
+              </option>
+            );
           })}
       </Form.Select>
       <Form.Control.Feedback type="invalid">
-        <>{props.dataExchange.errors?.[props.data.name]?.message}</>
+        {props.dataExchange.errors?.[props.data.name]?.message}
       </Form.Control.Feedback>
     </Form.Group>
   );
